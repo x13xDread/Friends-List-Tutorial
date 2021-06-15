e763371d682fd34cb91ea35dd0d5b86c48eb06d1
Miles <vorvon4@gmail.com>
2021-06-15 10:59:35 -0400

    Began DB association
    Started associating friends db with the user db, allowing individual users to have their own friends list

--- friends/db/schema.rb
+++ friends/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_122108) do
+ActiveRecord::Schema.define(version: 2021_06_15_143632) do
 
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_06_15_122108) do
     t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
